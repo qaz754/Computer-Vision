6681576596115a5818ac836a50c0964b480f4f24
Youngwook <30427798+yk287@users.noreply.github.com>
2018-12-16 01:44:24 -0800

    Add files via upload
    Added plots

--- MNIST/main.py
+++ MNIST/main.py
@@ -15,6 +15,7 @@ from torchvision import datasets, transforms
 
 from network import Network
 
+
 #Define a transform to normalize the data
 transform = transforms.Compose([transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
@@ -24,20 +25,20 @@ transform = transforms.Compose([transforms.ToTensor(),
 trainset = datasets.MNIST('MNIST_data/', download=True, train= True, transform = transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
 
-testset = datasets.MNIST('MNIST_data', download=False, train=False, transform = transform)
-testloader = torch.utils.data.DataLoader(testset, batch_size = 64, shuffle=False)
+testset = datasets.MNIST('MNIST_data/', download=False, train=False, transform = transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size = 1, shuffle=False)
 
 import trainer
 
 #Hyperparameters for our network
 
 output_size = 10
-model = Network(3, output_size, 0.4)
+model = Network(3, output_size, 0.6)
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.003)
+optimizer = optim.SGD(model.parameters(), lr=0.001)
 
-epochs = 10
+epochs = 15
 print_every = 40
 steps = 0
 
@@ -61,6 +62,7 @@ with torch.no_grad():
 ps = F.softmax(logits, dim=1)
 
 outputs = []
+
 for images, labels in iter(testloader):
 
     #images = images.view(images.size()[0], -1)
@@ -72,27 +74,11 @@ for images, labels in iter(testloader):
 
     outputs.append(predictions)
 
-#TODO get the test error
-#TODO make the code more modular
-#TODO implement augementation
-#TODO implement dropouts
-
-
-
-#import cv2
-
-#cv_img = cv2.imshow('test', images[0].squeeze().numpy())
-#cv2.waitKey()
-
-##
-#Graphing function
-##
-
 import util
 
 y_label = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
-img = images[9]
-util.pred_plotter(img, ps[9], y_label)
+img = images
+util.pred_plotter(img, predictions, y_label)
 
 
 def accuracy(net, loader):
@@ -122,3 +108,24 @@ images, labels = next(iter(trainloader))
 output = model.forward(images)
 
 _, prediction = torch.max(output.data, 1)
+
+
+#Confusion matrix
+
+confusion = torch.zeros(output_size, output_size)
+
+for images, labels in iter(testloader):
+
+    optimizer.zero_grad()
+
+    output = model(images)
+    _, prediction = torch.max(output.data, 1)
+
+    confusion[prediction.item()][labels.item()] += 1
+
+for i in range(output_size):
+
+    confusion[i] = confusion[i] / confusion[i].sum()
+
+util.confusion_plot(confusion.cpu(), list(y_label))
+
