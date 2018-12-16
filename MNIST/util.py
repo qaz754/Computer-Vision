2971b11973611e4aed0b955c36158851ebcc5f63
Youngwook <30427798+yk287@users.noreply.github.com>
2018-12-16 01:11:41 -0800

    Add files via upload
    Added a function that helps with plotting confusion matrix.

--- MNIST/util.py
+++ MNIST/util.py
@@ -3,6 +3,34 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
+
+import matplotlib.ticker as ticker
+
+def confusion_plot(matrix, y_category):
+    '''
+    A function that plots a confusion matrix
+    :param matrix: Confusion matrix
+    :param y_category: Names of categories.
+    :return: NA
+    '''
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    cax = ax.matshow(matrix.numpy())
+    fig.colorbar(cax)
+
+    # Set up axes
+    ax.set_xticklabels([''] + y_category, rotation=90)
+    ax.set_yticklabels([''] + y_category)
+
+    # Force label at every tick
+    ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+    ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    # sphinx_gallery_thumbnail_number = 2
+    plt.show()
+
+
 def accuracy(net, loader):
     '''
     A function that returns total number of correct predictions and total comparisons
