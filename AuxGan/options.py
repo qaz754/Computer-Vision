3ddf6be668662a1b84e9b2b2c14c5b32bfa6e1ec
Youngwook <30427798+yk287@users.noreply.github.com>
2019-03-02 17:37:58 -0800

    Add files via upload

--- AuxGan/options.py
+++ AuxGan/options.py
@@ -18,20 +18,20 @@ class options():
         self.parser.add_argument('--beta1', type=int, nargs='?', default=0.5, help='values for beta1')
         self.parser.add_argument('--beta2', type=int, nargs='?', default=0.999, help='values for beta2')
 
-        self.parser.add_argument('--lrelu_val', type=int, nargs='?', default=0.20, help='leaky Relu Value')
+        self.parser.add_argument('--lrelu_val', type=int, nargs='?', default=0.01, help='leaky Relu Value')
 
         #AUXGan options
         self.parser.add_argument('--num_classes', type=int, nargs='?', default=10, help='WGAN Gradient Penalty value')
 
         #Discriminator Options
-        self.parser.add_argument('--D_hidden', type=int, nargs='+', default=[512, 256, 128], help='hidden layer configuration in a list form for D')
+        self.parser.add_argument('--D_hidden', type=int, nargs='+', default=[256, 256], help='hidden layer configuration in a list form for D')
         self.parser.add_argument('--D_activation', type=str, nargs='?', default='lrelu', help='Activation function for the discriminator')
         self.parser.add_argument('--D_input_size', type=int, nargs='?', default=784, help='size of input for the discriminator')
         self.parser.add_argument('--D_output_size', type=int, nargs='?', default=1, help='size of output for the discriminator')
 
         #Generator Options
-        self.parser.add_argument('--G_hidden', type=int, nargs='+', default=[128, 256, 512, 1024], help='hidden layer configuration in a list form for G')
-        self.parser.add_argument('--G_activation', type=str, nargs='?', default='lrelu', help='Activation function for the generator')
+        self.parser.add_argument('--G_hidden', type=int, nargs='+', default=[1024, 1024], help='hidden layer configuration in a list form for G')
+        self.parser.add_argument('--G_activation', type=str, nargs='?', default='relu', help='Activation function for the generator')
         self.parser.add_argument('--noise_dim', type=int, nargs='?', default=96, help='size of noise input for the generator')
         self.parser.add_argument('--G_output_size', type=int, nargs='?', default=784, help='size of output for the discriminator')
         self.parser.add_argument('--G_out_activation', type=str, nargs='?', default='tanh', help='final output activator')
